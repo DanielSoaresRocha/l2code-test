854a4a80522fc25a3886ecd24ad3ca73fbfd4f20
unknown <legoliga123@gmail.com>
2025-03-03 15:22:41 -0300

    Filters Implemented

--- src/app/modules/contacts/components/search/search.component.ts
+++ src/app/modules/contacts/components/search/search.component.ts
@@ -1,6 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { UiSwitchModule } from 'ngx-ui-switch';
 import { FormateDatePipe } from '../../../../shared/pipe/formate-date.pipe';
 import { PhoneNumberPipe } from '../../../../shared/pipe/phone-number.pipe';
 import { Contact } from '../../model';
@@ -8,12 +17,23 @@ import { ContactsService } from '../../services/contacts.service';
 
 @Component({
   selector: 'app-search',
-  imports: [CommonModule, FormateDatePipe, PhoneNumberPipe],
+  imports: [
+    FormsModule,
+    CommonModule,
+    FormateDatePipe,
+    PhoneNumberPipe,
+    UiSwitchModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
+  providers: [provideNgxMask()],
 })
 export class SearchComponent implements OnInit {
   contacts!: Contact[];
+  contactsFilter!: Contact[];
+  filterForm!: FormGroup;
 
   constructor(
     private contactService: ContactsService,
@@ -22,6 +42,26 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.contacts = this.contactService.getData();
+    this.contactsFilter = this.contactService.getData();
+    this.createForm();
+  }
+
+  createForm() {
+    this.filterForm = new FormGroup({
+      nome: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      celular: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\d{11}$/),
+      ]),
+      telefone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\d{11}$/),
+      ]),
+      favorito: new FormControl(false),
+      ativo: new FormControl(false),
+      dateCadastro: new FormControl(null),
+    });
   }
 
   deleteContact(contact: Contact) {
@@ -36,7 +76,7 @@ export class SearchComponent implements OnInit {
     };
 
     this.contactService.update(contact);
-    this.contacts = this.contactService.getData();
+    this.reset();
   }
 
   inactiveContact(contact: Contact) {
@@ -46,7 +86,13 @@ export class SearchComponent implements OnInit {
     };
 
     this.contactService.update(contact);
+    this.reset();
+  }
+
+  reset() {
     this.contacts = this.contactService.getData();
+    this.contactsFilter = this.contactService.getData();
+    this.filterForm.reset();
   }
 
   updateContact(phoneNumber: number) {
@@ -55,7 +101,61 @@ export class SearchComponent implements OnInit {
     });
   }
 
+  filter() {
+    this.contactsFilter = this.contacts;
+    const filter: Contact = this.filterForm.getRawValue();
+
+    if (filter.favorito)
+      this.contactsFilter = this.contactsFilter.filter(
+        (c) => c.favorito == true
+      );
+
+    if (filter.ativo)
+      this.contactsFilter = this.contactsFilter.filter((c) => c.ativo == false);
+
+    if (filter.nome)
+      this.contactsFilter = this.contactsFilter.filter((c) =>
+        c.nome.includes(filter.nome)
+      );
+
+    if (filter.email)
+      this.contactsFilter = this.contactsFilter.filter((c) =>
+        c.email.includes(filter.email)
+      );
+
+    if (filter.celular)
+      this.contactsFilter = this.contactsFilter.filter((c) =>
+        c.celular.toString().includes(filter.celular.toString())
+      );
+    if (filter.telefone)
+      this.contactsFilter = this.contactsFilter.filter((c) =>
+        c.telefone.toString().includes(filter.telefone.toString())
+      );
+  }
+
   goToRegister() {
     this.router.navigate(['contacts/register']);
+  }
+
+  get nome() {
+    return this.filterForm.controls['nome'];
+  }
+  get email() {
+    return this.filterForm.controls['email'];
+  }
+  get celular() {
+    return this.filterForm.controls['celular'];
+  }
+  get telefone() {
+    return this.filterForm.controls['telefone'];
+  }
+  get dateCadastro() {
+    return this.filterForm.controls['dateCadastro'];
+  }
+  get ativo() {
+    return this.filterForm.controls['ativo'];
+  }
+  get favorito() {
+    return this.filterForm.controls['favorito'];
   }
 }
